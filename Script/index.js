57167d04df3d11924beb46821178cd3ac3b558dc
Diogo Antonny <Diogojp202@gmail.com>
2022-10-27 16:47:34 -0300

    problema de tela resolvido
    problema de tela resolvido
    window.addEventListener('resize',(matrixRain))

---
+++ Script/index.js
@@ -0,0 +1,32 @@
+function matrixRain() {
+    const c = document.getElementById('matrix');
+    const ctx = c.getContext('2d');
+
+    c.height = 200;
+    c.width = window.innerWidth;
+
+    const letters = ["日","ﾊ","ﾐ","ﾋ","ｰ","ｳ","ｼ","ﾅ","ﾓ","ﾆ","ｻ","ﾜ","ﾂ","ｵ","ﾘ","ｱ","ﾎ","ﾃ","ﾏ","ｹ","ﾒ","ｴ","ｶ","ｷ","ﾑ","ﾕ","ﾗ","ｾ","ﾈ","ｽ","ﾀ","ﾇ","ﾍ",":","・",".","=","*","+","-","<",">","¦","｜","ﾘ"];
+    const fontSize = 18;
+    const columns = c.width / fontSize;
+    const drops = new Array(Math.floor(columns)).fill(1);
+    function draw(){
+        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+        ctx.fillRect(0, 0, c.width, c.height);
+        ctx.fillStyle = "#0F0";
+        ctx.font = `${fontSize}px arial`;
+        
+        for(let i = 0; i < drops.length; i++){
+            let text = letters[Math.floor(Math.random()*letters.length)];
+
+            ctx.fillText(text, i * fontSize, drops[i] * fontSize); 
+            drops[i] ++;
+            if(drops[i] * fontSize > c.height && Math.random() > 0.99){
+                drops[i] = 0;
+            }
+        }
+        window.requestAnimationFrame(draw);
+    }
+    window.addEventListener('resize',(matrixRain))
+    draw();
+}
+matrixRain();
