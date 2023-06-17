7c96575babffb8ac7821955a55a4884711735c21
Diogo Antonny <Diogojp202@gmail.com>
2023-06-17 19:10:05 -0300

    scripts

--- Script/index.js
+++ Script/index.js
@@ -35,9 +35,7 @@ class animation{
             window.requestAnimationFrame(draw);
         }
 
-        window.addEventListener('resize', () => {
-            this.matrixRain();
-        });
+        window.addEventListener('resize', () => this.matrixRain());
 
         draw();
     }
