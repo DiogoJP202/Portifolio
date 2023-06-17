4372374cb3384bae088b21f5741604bfbf1e5550
Diogo Antonny <Diogojp202@gmail.com>
2023-06-17 18:07:46 -0300

    CONCLUIDO
    Todas as funcoes foram transcristas para classes
    Algumas animacoes adicionadas

--- Script/index.js
+++ Script/index.js
@@ -1,113 +1,144 @@
-function matrixRain() {
-    const c = document.getElementById('matrix');
-    const ctx = c.getContext('2d');
-    const screen = document.getElementsByTagName('body')[0];
+class animation{
+    matrixRain(){
+        const c = document.getElementById('matrix');
+        const ctx = c.getContext('2d');
+        const screen = document.querySelector('body');
+    
+        c.height = 380;
+        c.width = screen.offsetWidth; // Get Element Width;
+        // Is like window.innerWidth;
 
-    c.height = 380;
-    c.width = screen.offsetWidth;
-
-    const letters = ["日","ﾊ","ﾐ","ﾋ","ｰ","ｳ","ｼ","ﾅ","ﾓ","ﾆ","ｻ","ﾜ","ﾂ","ｵ","ﾘ","ｱ","ﾎ","ﾃ","ﾏ","ｹ","ﾒ","ｴ","ｶ","ｷ","ﾑ","ﾕ","ﾗ","ｾ","ﾈ","ｽ","ﾀ","ﾇ","ﾍ",":","・",".","=","*","+","-","<",">","¦","｜","ﾘ"];
-    const fontSize = 18;
-    const columns = c.width / fontSize;
-    const drops = new Array(Math.floor(columns)).fill(1);
-    function draw(){
-        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-        ctx.fillRect(0, 0, c.width, c.height);
-        ctx.fillStyle = "#0F0";
-        ctx.font = `${fontSize}px arial`;
+        const letters = ["日","ﾊ","ﾐ","ﾋ","ｰ","ｳ","ｼ","ﾅ","ﾓ","ﾆ","ｻ","ﾜ","ﾂ","ｵ","ﾘ","ｱ","ﾎ","ﾃ","ﾏ","ｹ","ﾒ","ｴ","ｶ","ｷ","ﾑ","ﾕ","ﾗ","ｾ","ﾈ","ｽ","ﾀ","ﾇ","ﾍ",":","・",".","=","*","+","-","<",">","¦","｜","ﾘ"];
+        const fontSize = 18;
+        const columns = c.width / fontSize;
+        const drops = new Array(Math.floor(columns)).fill(1);
         
-        for(let i = 0; i < drops.length; i++){
-            let text = letters[Math.floor(Math.random()*letters.length)];
-
-            ctx.fillText(text, i * fontSize, drops[i] * fontSize); 
-            drops[i] ++;
-            if(drops[i] * fontSize > c.height && Math.random() > 0.99){
-                drops[i] = 0;
+        function draw(){
+            ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+            ctx.fillRect(0, 0, c.width, c.height);
+            ctx.fillStyle = "#0F0";
+            ctx.font = `${fontSize}px arial`;
+            
+            for(let i = 0; i < drops.length; i++){
+                let text = letters[Math.floor(Math.random()*letters.length)];
+    
+                ctx.fillText(text, i * fontSize, drops[i] * fontSize); 
+                drops[i] ++;
+                if(drops[i] * fontSize > c.height && Math.random() > 0.99){
+                    drops[i] = 0;
+                }
             }
+            requestAnimationFrame(draw);
         }
-        window.requestAnimationFrame(draw);
-    }
-    window.addEventListener('resize',(matrixRain))
-    draw();
-}
 
-function FontRaibow(){
-    const fonte = document.querySelector('.fontRaibow');
+        window.addEventListener('resize', () => {
+            clearRect(0, 0, ctx.width, ctx.height);
+            this.matrixRain();
+        });
 
-    let timer = setInterval(() => {
-        setTimeout(()=>{
-            fonte.style.color = `${generateColor()}`;
-        }, 500)
-    }, 1000)
-    window.requestAnimationFrame(FontRaibow);
-}
-
-function generateColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    
-    return color; 
-}
-
-function hands(){
-    const hands = document.querySelectorAll('.hand');
-    let contador = 0;
-
-    hands[1].style.transform = "scale(-1)";
-    hands[0].style.transform = "scale(-1)";
-    
-    function handLeft(){
-        hands[0].style.transform = "rotate(30deg)";
-        hands[1].style.transform = "rotate(-30deg)";
-    }
-    
-    function handright(){
-        hands[0].style.transform = "rotate(-30deg)";
-        hands[1].style.transform = "rotate(30deg)";
+        draw();
     }
 
-    setInterval(()=>{
-        if(contador % 2 === 0){
-            handLeft();
-        } else {
-            handright();
+    static generateColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
         }
-        contador ++;
-    }, 500)
-}
+        
+        return color; 
+    }
 
-function showMore(){
-    let contador = 1;
-    let contador1 = 1;
+    FontRaibow(){
+        const fonte = document.querySelector('.fontRaibow');
 
-    document.addEventListener('click', event => {
-        element = event.target;
-
-        if(element.classList.contains('showMore')){
-            const projects = document.querySelectorAll('.Project');
-
+        fonte.style.color = `${animation.generateColor()}`;
+        setInterval(() => setTimeout(() => fonte.style.color = `${animation.generateColor()}`, 50), 50);
+    }
+    
+    hands(){
+        const hands = document.querySelectorAll('.hand');
+        let contador = 0;
+    
+        hands[1].style.transform = "scale(-1)";
+        hands[0].style.transform = "scale(-1)";
+        
+        function handLeft(){
+            hands[0].style.transform = "rotate(30deg)";
+            hands[1].style.transform = "rotate(-30deg)";
+        }
+        
+        function handright(){
+            hands[0].style.transform = "rotate(-30deg)";
+            hands[1].style.transform = "rotate(30deg)";
+        }
+    
+        setInterval(()=>{
+            if(contador % 2 === 0){
+                handLeft();
+            } else {
+                handright();
+            }
             contador ++;
-            projects[contador].style.display = 'block';
+        }, 300)
+    }
+    
+    showMore(){
+        let contador = [1,1];
+    
+        document.addEventListener('click', event => {
+            const element = event.target;
+    
+            if(element.classList.contains('showMore')){
+                const projects = document.querySelectorAll('.Project');
+    
+                contador[0] ++;
+                projects[contador[0]].style.display = 'block';
+    
+                if(contador[0] > 3) element.style.display = 'none';
+            }
+    
+            if(element.classList.contains('showMore1')){
+                const projects = document.querySelectorAll('.Project1');
+    
+                contador[1] ++;
+                projects[contador[1]].style.display = 'block';
+    
+                if(contador[1] > 12) element.style.display = 'none';
+            }
+        })
+    }
 
-            if(contador > 3) element.style.display = 'none';
+    linkAnimation(){
+        const links = document.querySelectorAll('.linkR');
+        let contador = 0;
+
+        function left(){
+            links[0].style.transform = "rotate(-30deg)";
+            links[1].style.transform = "rotate(-30deg)";
+        }
+        
+        function right(){
+            links[0].style.transform = "rotate(30deg)";
+            links[1].style.transform = "rotate(30deg)";
         }
 
-        if(element.classList.contains('showMore1')){
-            const projects = document.querySelectorAll('.Project1');
-
-            contador1 ++;
-            projects[contador1].style.display = 'block';
-
-            if(contador1 > 12) element.style.display = 'none';
-        }
-    })
+        setInterval(()=>{
+            if(contador % 2 === 0){
+                left();
+            } else {
+                right();
+            }
+            contador ++;
+        }, 2000)
+    }
 }
 
-FontRaibow();
-matrixRain();
-hands();
-showMore()
+const animations = new animation();
+
+animations.matrixRain();
+animations.showMore();
+animations.hands();
+animations.FontRaibow();
+animations.linkAnimation();
